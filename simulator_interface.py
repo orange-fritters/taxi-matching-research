0b6ca589787240edfeac7709812c7acfc73d9351
juuc <ucheriya@snu.ac.kr>
2023-05-25 22:53:06 +0900

    🍻 feat: Complete some functions and make TODO

--- simulator_interface.py
+++ simulator_interface.py
@@ -1,11 +1,13 @@
 from abc import *
-from typing import List, Tuple, TypedDict
+from typing import List, Tuple, TypedDict, Dict
 import pandas as pd
 import numpy as np
 from weight_assigning_model import Model
+from scipy.optimize import linear_sum_assignment
 
 
 class Vehicle(TypedDict):
+    id: int
     destination_loc: int
     ETA: int
     curr_loc: int
@@ -31,12 +33,14 @@ class SimulatorInterface(metaclass=ABCMeta):
         Simulator class for simulation
         """
         self.data: pd.DataFrame = ...
-        self.order: List[Request] = ...
-        self.vehicles: List[Vehicle] = ...
-        self.waiting_times: List[int] = ...
+        self.requests: Dict[int, Request] = ... # key : ID, value: Request
+        self.vehicles: Dict[int, Vehicle] = ... # key : ID, value: Vehicle
+        self.waiting_times: Dict[int, int] = ... # key : request ID, value : waiting time
         self.model = Model()
-        self.total_time = total_time        # 총합 시간 (첨두 비첨두 따라 다르므로)
-        self.time_window = time_window      # batch interval
+        self.total_time = total_time        # 총합 시간 (첨두 비첨두 따라 다르므로, 단위 : min)
+        self.time_window = time_window      # batch interval (단위 : min)
+        self.counter = 0                    # self.data에서 현재 추적중인 행
+        self.matched_number = 0             # 매칭된 요청의 수(최종 리턴 값)
 
     @abstractmethod
     def update_vehicle_status(self):
@@ -49,6 +53,12 @@ class SimulatorInterface(metaclass=ABCMeta):
         Update:
             self.vehicles
         """
+        for vehicle in self.vehicles:
+            if vehicle.ETA > 0:
+                vehicle.ETA -= 1
+            # if vehicle.ETA == 0: <- filter_vehicles에서 이미 available 처리됨
+
+        # TODO : Update Vehicle's availability by work started and ended)
 
     @abstractmethod
     def update_vehicle_object(self,
@@ -68,28 +78,48 @@ class SimulatorInterface(metaclass=ABCMeta):
           self.requests
           self.vehicles
         """
+        for veh_id, req_id in matched_pair:
+            self.vehicles[veh_id].destination_loc = self.requests[req_id].destination_loc
+            self.vehicles[veh_id].ETA = self.requests[req_id].travel_time
+            del self.requests[req_id]
+
+        for req_id in self.requests: # unmatched requests are in self.requests
+            self.waiting_times[req_id] += self.time_window
+
 
     @abstractmethod
-    def update_order(self,
-                     data: pd.DataFrame,
+    def update_requests(self,
                      time: int) -> None:
         """
-        Update the order variable time by time
+        Update the requests variable and waiting times for some requests time by time 
         """
-        self.order = ...
+        data = self.data
+        while data.loc[self.counter, "희망일시"] <= time:
+            req = Request()
+            req.id = self.counter
+            req.travel_time = data.loc[self.counter, "하차시간"] - data.loc[self.counter, "탑승시간"]
+            req.request_time = data.loc[self.counter, "희망일시"]
+            req.origin_loc = data.loc[self.counter, "출발동"]
+            req.destination_loc = data.loc[self.counter, "목적동"]
+            self.requests[req.id] = req
+            self.waiting_times[req.id] = 0
+            self.counter += 1
+
 
     @abstractmethod
     def filter_vehicles(self) -> None:
         """
-        Modify self.vehicle's available parameter for current orders
+        Modify self.vehicle's available parameter for current requests
         Args:
-            self.order: list of requests
+            self.requests: list of requests
             self.vehicles: list of all vehicles
         Returns:
             self.vehicles : modified self.vehicles
         """
-        pass
-
+        for vehicle in self.vehicles.items():
+            if not vehicle.available and vehicle.ETA <= 5:
+                vehicle.available = True
+                
 
     @abstractmethod
     def assign_weight(self) -> np.array:
@@ -97,12 +127,12 @@ class SimulatorInterface(metaclass=ABCMeta):
         Using the model, return the weight
         Only use 
         Args:
-            self.order: list of requests
-            self.vehicles: list of all vehicles
+            self.requests: dict of requests
+            self.vehicles: dict of all vehicles
         Returns:
             weight : modified self.vehicles
         """
-        weight: np.array = self.model.assign()
+        weight: np.array = self.model.assign(self.requests, self.vehicles)
         return weight
 
     @abstractmethod
@@ -113,10 +143,15 @@ class SimulatorInterface(metaclass=ABCMeta):
             weight: np.array
         Variables:
             self.vehicles
-            self.order
+            self.requests
         Returns:
-            matched_pair : List[Tuple] = ...  # Tuple : (veh_idx, request_id)
+            matched_pair : List[Tuple] = ...  # Tuple : (veh_id, request_id)
         """
+        row_ind, col_ind = linear_sum_assignment(weight)
+        # TODO : Implement KM Algorithm
+        # weight의 row에 request, col에 vehicle을 배치할 것이므로
+        # col_ind가 각 request별 배정된 vehicle의 index가 될 것임
+
         matched_pair: List[Tuple] = ...  # Tuple : (veh_idx, request_id)
         return matched_pair
 
@@ -133,21 +168,22 @@ class SimulatorInterface(metaclass=ABCMeta):
         Raises:
             KeyError: Raises an exception.
         """
-        self.watiting_times = ...
+        self.waiting_times = ...
 
     @abstractmethod
-    def match(self, batch, match) -> None:
+    def match(self) -> None:
         """
         Conduct matching process
         Variable:
             self.vehicles
-            self.order
+            self.requests
         Update:
             self.vehicles
-            self.order
+            self.requests
         """
         weight = self.assign_weight()
         matched_pair = self.KM_algorithm(weight=weight)
+        self.matched_number += len(matched_pair)
         self.log(matched_pair=matched_pair)
         self.update_vehicle_object(matched_pair=matched_pair)
 
@@ -159,7 +195,7 @@ class SimulatorInterface(metaclass=ABCMeta):
         tmp: int = 0
         for t in range(self.total_time):
             self.update_vehicle_status()
-            self.update_order(self.data, t)
+            self.update_requests(t)
             if (tmp < self.time_window):
                 tmp += 1
             else:
@@ -167,3 +203,4 @@ class SimulatorInterface(metaclass=ABCMeta):
                 self.match()
                 tmp = 0
         return self.waiting_times
+
--- weight_assigning_model.py
+++ weight_assigning_model.py
@@ -1,9 +1,29 @@
 import numpy as np
+from typing import TypedDict, Dict, List
 
+class Vehicle(TypedDict):
+    id: int
+    destination_loc: int
+    ETA: int
+    curr_loc: int
+    curr_time: int
+    available: bool
+    working: bool
+
+
+class Request(TypedDict):
+    id: int
+    travel_time: int      # 이동한 시간 == 하차 시간 - 탑승 시간
+    request_time: int     # 요청 시각
+    origin_loc: int       # 출발동
+    destination_loc: int  # 목적동
 
 class Model:
     def __init__(self) -> None:
         pass
 
-    def assign(self) -> np.array:
-        pass
+    def assign(self, requests: Dict[int, Request], vehicles: Dict[int, Vehicle]) -> np.array:
+        # TODO: For each requests, assign a weight for each vehicle
+        # Make a matrix of weights (request X vehicle)
+        # CAUTION: Keep track of the order of requests and vehicles
+        return np.outer()
